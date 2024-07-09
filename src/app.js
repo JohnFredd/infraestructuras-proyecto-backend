35539351f0da5f37c081aa8c1428bfbc9ae0d9ed
John <john.freddy.belalcazar@correounivalle.edu.co>
2024-07-08 20:50:47 -0500

    solucione error con dev y sync

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/database');
-const logger = require('./logger');
+const logger = require('./config/logger');
 
 const app = express();
 const port = 3000;
